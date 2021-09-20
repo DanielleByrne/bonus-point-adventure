0b6f00149139f978ab53d287d3b2bb626a042b51
Danielle Byrne <DanielleByrne421@gmail.com>
2021-09-20 16:37:27 -0700

    planned out all the options for the game

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("cowa-bummer")
+// choose your own adventure type text game as a surprise for brandon
+// option 1 : really hungry  -> esthers kitchen for brunch OR not too hungry ->  chikfila breakfast
+// option 2 : play something new -> a way out on xbox OR something known and loved -> unmatched
+// option 3 : do something fun -> axe throwing OR look at something cool -> neon museum 
+// option 4 : eat out -> skinny fats OR eat in -> bibim kitchen OR wild card -> the hawaiian trifecta
+// option 5 : dessert> if yes, something classic -> boba OR something customizeable -> froyo 
