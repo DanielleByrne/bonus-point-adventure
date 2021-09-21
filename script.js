74d6644e9b067a08eea28375f2d2e6bd9b369f37
Danielle Byrne <DanielleByrne421@gmail.com>
2021-09-20 22:05:33 -0700

    added all the options, didn't create the state option for dessert or log the ones that were chosen yet

--- script.js
+++ script.js
@@ -13,43 +13,41 @@ let state = {};
 
 // the funciton that starts the game at the beginning
 function startGame() {
-    state = {}
-    showTextNode(1)
+  state = {};
+  showTextNode(1);
 }
 
 //funciton for showing the right text of whatever question we moved onto
 function showTextNode(textNodeIndex) {
-
-    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
-    textElement.innerText = textNode.text
-// loop to remove all the options so that we only fill the space with the ones we need for that choice
-    while(choiceButtonsElement.firstChild) {
-        choiceButtonsElement.removeChild(choiceButtonsElement.firstChild)
+  const textNode = textNodes.find((textNode) => textNode.id === textNodeIndex);
+  textElement.innerText = textNode.text;
+  // loop to remove all the options so that we only fill the space with the ones we need for that choice
+  while (choiceButtonsElement.firstChild) {
+    choiceButtonsElement.removeChild(choiceButtonsElement.firstChild);
+  }
+  // adding a button for each choice we have
+  textNode.options.forEach((option) => {
+    if (showOption(option)) {
+      const button = document.createElement("button");
+      button.innerText = option.text;
+      button.classList.add("button");
+      button.addEventListener("click", () => selectChoice(option));
+      choiceButtonsElement.appendChild(button);
+      
     }
-// adding a button for each choice we have
-    textNode.options.forEach(option => {
-        if (showOption(option)){
-            const button = document.createElement("button")
-            button.innerText = option.text
-            button.classList.add("button")
-            button.addEventListener("click" , () => selectOption(option))
-            choiceButtonsElement.appendChild(button)
-        }
-    })
+  }); 
 }
 
-
-function showOption(option){
- return true
+function showOption(option) {
+  return true
 }
-
 
 //function for when we select an option
-function selectChoice(choice) {}
+function selectChoice(choice) {
+const nextTextNodeId = choice.nextText
+ showTextNode(nextTextNodeId)
 
-
-
-
+}
 
 // the array of objects of what is going to fill the text box and option buttons, the id is what the next text will lead to and the number of options is how many buttons there will be
 const textNodes = [
@@ -72,10 +70,61 @@ const textNodes = [
         nextText: 3,
       },
       {
-        text: "I could eat but not a whole lot",
+        text: "I could eat, but not a whole lot",
         nextText: 3,
       },
     ],
+  },
+  {
+    id: 3,
+    text: "Now it's time for the first activity, I think it would be fun to play some kind of game, please choose if you want",
+    options: [
+      {
+        text: "Something new",
+        nextText: 4,
+      },
+      {
+        text: "Something known and loved",
+        nextText: 4,
+      },
+    ],
+  },
+  {
+    id: 4,
+    text: "Time for activity 2! What sounds like more fun?",
+    options: [
+      {
+        text: "Go see something cool",
+        nextText: 5,
+      },
+      {
+        text: "Do something entertaining",
+        nextText: 5,
+      },
+    ],
+  },
+  {
+    id: 5,
+    text: "It's food o'clock :)",
+    options: [
+      {
+        text: "Go somewhere nice",
+        nextText: 6,
+      },
+      {
+        text: "Order-in",
+        nextText: 6,
+      },
+      {
+        text: "Wild Card",
+        nextText: 6,
+      },
+    ],
+  },
+  {
+    id: 6,
+    text: "That's all! Have a good bonus point day <3",
+    options: [],
   },
 ];
 
